9933ce40711185e73a07d1ce52965f9b373429fa
buhang <yifan253421937@outlook.com>
2024-06-18 00:14:10 +0800

    删除菜品业务代码

---
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
@@ -0,0 +1,19 @@
+package com.sky.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@ApiModel(description = "员工登录时传递的数据模型") // 对实体的说明
+public class EmployeeLoginDTO implements Serializable {
+
+    @ApiModelProperty("用户名") // 对属性的说明
+    private String username;
+
+    @ApiModelProperty("密码")
+    private String password;
+
+}
