321c838a6fc4f3358bbbd7d4abf1c60ae9b4524d
buhang <yifan253421937@outlook.com>
2024-06-27 00:55:25 +0800

    清空购物车业务代码开发

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -46,4 +46,17 @@ public class ShoppingCartController {
         List<ShoppingCart> list = shoppingCartService.showShoppingCart();
         return Result.success(list);
     }
+
+    /**
+     * 清空购物车
+     * @return
+     */
+    @DeleteMapping("/clean")
+    @ApiOperation("清空购物车")
+    public Result cleanAll(){
+        log.info("清空购物车");
+
+        shoppingCartService.cleanShoppingCart();
+        return Result.success();
+    }
 }
